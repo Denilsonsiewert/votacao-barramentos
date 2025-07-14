af55f4d77c2da6d4fcef9cb611cb283a088c596b
Denilsonsiewert <39243863+Denilsonsiewert@users.noreply.github.com>
2025-07-14 17:06:59 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import VotacaoColaboradorDestaque from './VotacaoColaboradorDestaque';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <VotacaoColaboradorDestaque />
+  </React.StrictMode>
+);
